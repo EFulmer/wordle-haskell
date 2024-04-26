822d1b40144812a62ff5628fc7423de399aa4ce6
Eric R. Fulmer <2952996+EFulmer@users.noreply.github.com>
2024-04-26 16:09:51 -0400

    main now prints out the word of the day

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,36 @@
 module Main where
 
+import Control.Monad
 import Data.Char
 import System.IO
+import System.Random
+
+randGen = mkStdGen 99
+
+select :: StdGen -> [String] -> String
+select gen ss = ss !! x
+    -- TODO: figure out how to keep the generator too
+    where (x, _) = randomR (0, n) gen
+          n = length ss
+
+-- get all the words
+getStandardWordList :: IO [String]
+getStandardWordList = do
+    usrDict <- readFile "/usr/share/dict/words"
+    return $ lines usrDict
+
+-- half the point of the `normalizeWords` and `filterWords` functions is so I can add more preprocessing in there later
+filterWords :: [String] -> [String]
+filterWords ss = filter (\w -> length w == 5) ss
+
+normalizeWords :: [String] -> [String]
+normalizeWords = map $ map toLower
+
+preprocess = liftM $ (filterWords .normalizeWords)
 
 main :: IO ()
 main = do
-    usrDict <- readFile "/usr/share/dict/words"
-    let words = lines usrDict
-    let validWords = filter (\w -> length w == 5) words
-    let normalizedValidWords = map (map toLower) validWords
-    print $ take 5 words
-    putStrLn $ head validWords
-    mapM putStrLn $ take 5 normalizedValidWords
-    return ()
+    words <- preprocess $ getStandardWordList
+    mapM putStrLn $ take 5 words
+    let word = select randGen words
+    putStrLn $ "The wordle of the day is " ++ word
