664659a262f015ef54916dde6766d78ca19e9990
Eric R. Fulmer <2952996+EFulmer@users.noreply.github.com>
2024-04-26 14:58:34 -0400

    print out all length 5 words from /usr/share/dict/words

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,15 @@
 module Main where
 
+import Data.Char
+import System.IO
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    usrDict <- readFile "/usr/share/dict/words"
+    let words = lines usrDict
+    let validWords = filter (\w -> length w == 5) words
+    let normalizedValidWords = map (map toLower) validWords
+    print $ take 5 words
+    putStrLn $ head validWords
+    mapM putStrLn $ take 5 normalizedValidWords
+    return ()
