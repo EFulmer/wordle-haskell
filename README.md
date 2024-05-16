439a73ced2c34d234f987afd8b0f7a0fee84765d
Eric R. Fulmer <2952996+EFulmer@users.noreply.github.com>
2024-05-15 20:14:03 -0400

    use Data.Text instead of String

--- README.md
+++ README.md
@@ -8,7 +8,6 @@ Wordle clone in Haskell. Terminal-based!
 #### TO-DO
 
 * Makefile that builds, lints, installs dependencies, etc.
-* Use `Text` data type instead of Haskell's built-in `String`
 * Switch from partial functions like `!!` to safe versions
 * Tests
 * Handle the case of a negative `roundsLeft` being passed to `playWordle`
--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Control.Monad
 import Data.Char
 import qualified Data.Set as Set -- required install
+import qualified Data.Text as T
+import qualified Data.Text.IO as T.IO
 import System.Console.ANSI hiding (Color, Green, Yellow) -- required install
 import System.IO
 import System.Random -- required install
@@ -17,75 +20,76 @@ data LetterMatch = LetterMatch
   deriving (Show)
 
 -- Setup (get the word) functions
-select :: StdGen -> [String] -> String
+select :: StdGen -> [T.Text] -> T.Text
 select gen ss = ss !! x
   where
     -- TODO: figure out how to keep the generator too
     (x, _) = randomR (0, n) gen
     n = length ss
 
-getStandardWordList :: IO [String]
+getStandardWordList :: IO [T.Text]
 getStandardWordList = do
-  usrDict <- readFile "/usr/share/dict/words"
-  return $ lines usrDict
+  usrDict <- T.IO.readFile "/usr/share/dict/words"
+  return $ T.lines usrDict
 
-filterWords :: [String] -> [String]
-filterWords ss = filter (\s -> length s == 5 && all isAscii s) ss
+filterWords :: [T.Text] -> [T.Text]
+filterWords ss = filter (\s -> T.length s == 5 && T.all isAscii s) ss
 
-normalizeWords :: [String] -> [String]
-normalizeWords = map $ map toLower
+normalizeWords :: [T.Text] -> [T.Text]
+normalizeWords = fmap T.toLower
 
-preprocess :: IO [String] -> IO [String]
+preprocess :: IO [T.Text] -> IO [T.Text]
 preprocess = liftM $ (filterWords . normalizeWords)
 
 -- Game logic functions
-checkOneCharacter :: Char -> Int -> String -> LetterMatch
+checkOneCharacter :: Char -> Int -> T.Text -> LetterMatch
 checkOneCharacter guess pos answer
-  | answer !! pos == guess = LetterMatch {letter = guess, position = pos, color = Green}
-  | guess `elem` answer = LetterMatch {letter = guess, position = pos, color = Yellow}
+  | (T.index answer pos) == guess = LetterMatch {letter = guess, position = pos, color = Green}
+  | guess `T.elem` answer = LetterMatch {letter = guess, position = pos, color = Yellow}
   | otherwise = LetterMatch {letter = guess, position = pos, color = Gray}
 
-checkWholeWord :: String -> String -> [LetterMatch]
-checkWholeWord guess answer = [checkOneCharacter (guess !! i) i answer | i <- [0 .. 4]]
+checkWholeWord :: T.Text -> T.Text -> [LetterMatch]
+checkWholeWord guess answer = [checkOneCharacter (T.index guess i) i answer | i <- [0 .. 4]]
 
-guessValid :: String -> Set.Set String -> Bool
-guessValid guess dictionary = (length guess == 5) && (all isAscii guess) && guess `Set.member` dictionary
+guessValid :: T.Text -> Set.Set T.Text -> Bool
+guessValid guess dictionary = (T.length guess == 5) && (T.all isAscii guess) && guess `Set.member` dictionary
 
 isCorrect :: [LetterMatch] -> Bool
 isCorrect matches = all (\match -> (color match) == Green) matches
 
 -- Colorizing functions
-colorCode :: Color -> String
+colorCode :: Color -> T.Text
 colorCode Green = "\ESC[42m"
 colorCode Yellow = "\ESC[43m"
 colorCode Gray = "\ESC[100m"
 
-formatHint :: [LetterMatch] -> String
-formatHint matches = join $ map formatOne matches
-  where
-    formatOne match = colorCode (color match) ++ [letter match]
+formatOne :: LetterMatch -> T.Text
+formatOne match = T.snoc (colorCode (color match)) (letter match)
 
-strHint :: [LetterMatch] -> String
-strHint matches = "\ESC[107m" ++ formatHint matches ++ "\ESC[0m"
+formatHint :: [LetterMatch] -> T.Text
+formatHint matches = T.concat $ fmap formatOne matches
+
+strHint :: [LetterMatch] -> T.Text
+strHint matches = T.append (T.append "\ESC[107m" (formatHint matches)) "\ESC[0m"
 
 -- I/O functions
-askUser :: IO String
+askUser :: IO T.Text
 askUser = do
   getChars
 
-getChars :: IO String
+getChars :: IO T.Text
 getChars = do
   input <- getChar
   if input == '\n'
-    then return ""
+    then return $ T.pack ""
     else do
       rest <- getChars
-      return (input : rest)
+      return $ T.cons input rest
 
 -- Game loop
-playWordle :: String -> Set.Set String -> Int -> IO ()
+playWordle :: T.Text -> Set.Set T.Text -> Int -> IO ()
 playWordle answer dictionary roundsLeft
-  | roundsLeft == 0 = putStrLn "Sorry, you're out of guesses. The word was: " >> putStrLn answer >> putStrLn "Play another game soon!"
+  | roundsLeft == 0 = T.IO.putStrLn "Sorry, you're out of guesses. The word was: " >> T.IO.putStrLn answer >> T.IO.putStrLn "Play another game soon!"
   | otherwise = do
       putStr "> "
       hFlush stdout
@@ -94,7 +98,6 @@ playWordle answer dictionary roundsLeft
         then putStrLn "That's not a recognized 5-letter word. Please try again." >> playWordle answer dictionary roundsLeft
         else do
           let hint = checkWholeWord guess answer
-          -- putStrLn $ strHint hint
           printGuessHighlighted $ strHint hint
           if isCorrect hint
             then do
@@ -102,12 +105,12 @@ playWordle answer dictionary roundsLeft
             else do
               playWordle answer dictionary (roundsLeft - 1)
 
-printGuessHighlighted :: String -> IO ()
+printGuessHighlighted :: T.Text -> IO ()
 printGuessHighlighted word = do
   putStr $ cursorUpLineCode 1
   clearLine
   setCursorColumn 0
-  putStrLn word
+  T.IO.putStrLn word
 
 main :: IO ()
 main = do
